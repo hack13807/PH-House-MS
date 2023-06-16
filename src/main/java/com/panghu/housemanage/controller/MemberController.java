4d93f6283296d290aba69b251dfe9d9ee368942f
tanyuanzhi <564692972@qq.com>
2023-06-17 00:09:59 +0800

    Revert "表格示例"
    This reverts commit b3bf95667ceecfced2b544cef3d4950102ebd177.

--- src/main/java/com/panghu/housemanage/controller/MemberController.java
+++ src/main/java/com/panghu/housemanage/controller/MemberController.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -47,36 +47,6 @@ public class MemberController {
         model.addAttribute("page",pageResult);
         model.addAttribute("condition", Optional.ofNullable(condition).orElse(new MemberCondition(1)));
         return "member_list";
-    }
-
-    @RequestMapping("/data")
-    @ResponseBody
-    public Map<String, Object>getData() {
-        List<MemberVo> list = new ArrayList<>();
-        list.add(MemberVo.builder().memberId(1L).roomId(1L).memberName("房间1").build());
-        list.add(MemberVo.builder().memberId(2L).roomId(2L).memberName("房间2").build());
-        list.add(MemberVo.builder().memberId(3L).roomId(3L).memberName("房间3").build());
-        list.add(MemberVo.builder().memberId(4L).roomId(4L).memberName("房间4").build());
-        list.add(MemberVo.builder().memberId(5L).roomId(4L).memberName("房间4").build());
-        list.add(MemberVo.builder().memberId(6L).roomId(4L).memberName("房间4").build());
-        list.add(MemberVo.builder().memberId(7L).roomId(4L).memberName("房间4").build());
-        list.add(MemberVo.builder().memberId(8L).roomId(4L).memberName("房间4").build());
-        list.add(MemberVo.builder().memberId(9L).roomId(4L).memberName("房间4").build());
-        list.add(MemberVo.builder().memberId(10L).roomId(4L).memberName("房间4").build());
-        Map<String, Object> map = new HashMap<>();
-        map.put("code", "0000");
-        map.put("data", list);
-
-        map.put("total", "12");
-        map.put("rows", list);
-
-        return map;
-    }
-
-    @RequestMapping("/deleteData")
-    @ResponseBody
-    public void getData(Long id) {
-        System.out.println(id);
     }
 
 }

d294b244c1abcd1474fb7d35d97b64d8bd60fc12
hukun12306 <1690307988@qq.com>
2023-06-17 19:37:19 +0800

    Revert "update:新增用户列表页面"
    This reverts commit 72c0295a90f149f0474a5d5e64c83dedd6ca3559.

--- src/main/java/com/panghu/housemanage/service/UserService.java
+++ src/main/java/com/panghu/housemanage/service/UserService.java
@@ -1,16 +1,7 @@
 package com.panghu.housemanage.service;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.panghu.housemanage.pojo.po.User;
-import com.panghu.housemanage.pojo.vo.UserVo;
-
-import java.util.Map;
 
 public interface UserService {
-    User queryUser(User user); //登录
-
-    IPage<UserVo> listUser(Page<UserVo> page, Map<String, Object> params);//查询用户列表
-
-    //IPage<MemberVo> pageQueryMember(Page<MemberVo> page, Map<String, Object> params);
+     User queryUser(User user);
 }
--- src/main/java/com/panghu/housemanage/service/impl/UserServiceImpl.java
+++ src/main/java/com/panghu/housemanage/service/impl/UserServiceImpl.java
@@ -1,15 +1,10 @@
 package com.panghu.housemanage.service.impl;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.panghu.housemanage.dao.UserMapper;
 import com.panghu.housemanage.pojo.po.User;
-import com.panghu.housemanage.pojo.vo.UserVo;
 import com.panghu.housemanage.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,13 +12,7 @@ public class UserServiceImpl implements UserService {
     UserMapper userMapper;
 
     @Override
-    public User queryUser(User user) {
-        return userMapper.queryUser(user);
-    }
-
-    @Override
-    public IPage<UserVo> listUser(Page<UserVo> page, Map<String, Object> params) {
-        return userMapper.listUser(page, params);
-    }
-
+    public User queryUser(User user){
+        return  userMapper.queryUser(user);
+    };
 }

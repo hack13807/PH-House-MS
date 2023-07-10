42c34aa61733cf64392b4de4159997184b79881e
tanyuanzhi <564692972@qq.com>
2023-07-10 10:19:30 +0800

    feat: mysql 改造为 h2 数据库

--- src/main/resources/script/create_table.sql
+++ src/main/resources/script/create_table.sql
@@ -1,36 +1,32 @@
 DROP TABLE IF EXISTS `t_member`;
-CREATE TABLE `t_member`
-(
-    `id`      bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name`    char(4) CHARACTER SET utf8 COLLATE utf8_unicode_ci     NOT NULL DEFAULT '' COMMENT '房号',
-    `tel`     varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '关联t_member',
-    `idcard`  varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '身份证号',
-    `room_id` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '关联t_room',
-    `status`  tinyint(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'status：0正常  1退租',
-    PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 44 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `t_member` (
+    `id` bigint(20) AUTO_INCREMENT NOT NULL,
+    `name` char(4) DEFAULT '' NOT NULL COMMENT '房号',
+    `tel` varchar(20) DEFAULT '' NOT NULL COMMENT '关联t_member',
+    `idcard` varchar(30) DEFAULT '' NOT NULL COMMENT '身份证号',
+    `room_id` bigint(20) DEFAULT 0 NOT NULL COMMENT '关联t_room',
+    `status` tinyint(3) DEFAULT 0 NOT NULL COMMENT 'status：0正常  1退租',
+    PRIMARY KEY (`id`)
+);
 
 DROP TABLE IF EXISTS `t_room`;
-CREATE TABLE `t_room`
-(
-    `id`          bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `number`      char(4) CHARACTER SET utf8 COLLATE utf8_unicode_ci      NOT NULL DEFAULT '' COMMENT '房号',
-    `status`      tinyint(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT '房间状态  0待租  1出租中  2待处理',
-    `member_id`   bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '关联t_member',
-    `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '描述',
-    PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 175 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+CREATE TABLE `t_room` (
+  `id` bigint(20) AUTO_INCREMENT NOT NULL,
+  `number` char(4) DEFAULT '' NOT NULL COMMENT '房号',
+  `status` tinyint(3) DEFAULT 0 NOT NULL COMMENT '房间状态  0待租  1出租中  2待处理',
+  `member_id` bigint(20) DEFAULT 0 NOT NULL COMMENT '关联t_member',
+  `description` varchar(255) DEFAULT '' NOT NULL COMMENT '描述',
+  PRIMARY KEY (`id`)
+);
 
 DROP TABLE IF EXISTS `t_user`;
-CREATE TABLE `t_user`
-(
-    `id`      bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-    `number`  varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL DEFAULT NULL,
-    `name`    varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL DEFAULT NULL,
-    `phone`   varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL DEFAULT NULL,
-    `pwd`     varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NULL DEFAULT NULL,
-    `role_id` bigint NULL DEFAULT NULL COMMENT '关联t_role表',
-    `status`  tinyint NULL DEFAULT NULL COMMENT '0：正常  1：禁用',
-    PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_unicode_ci ROW_FORMAT = Dynamic;
-
+CREATE TABLE `t_user` (
+  `id` bigint AUTO_INCREMENT NOT NULL,
+  `number` varchar(20) DEFAULT NULL NULL,
+  `name` varchar(20) DEFAULT NULL NULL,
+  `phone` varchar(20) DEFAULT NULL NULL,
+  `pwd` varchar(20) DEFAULT NULL NULL,
+  `role_id` bigint DEFAULT NULL NULL COMMENT '关联t_role表',
+  `status` tinyint DEFAULT NULL NULL COMMENT '0：正常  1：禁用',
+  PRIMARY KEY (`id`)
+);
--- src/main/resources/script/ddl.sql
+++ src/main/resources/script/ddl.sql
@@ -1,14 +1,10 @@
-ALTER TABLE `t_room`
-DROP COLUMN `description`,
-ADD COLUMN `description` varchar(255) NOT NULL DEFAULT '' COMMENT '描述' AFTER `member_id`;
+ALTER TABLE `t_room` DROP COLUMN `description`;
+ALTER TABLE `t_room` ADD (`description` varchar(255) DEFAULT '' NOT NULL COMMENT '描述');
 
-ALTER TABLE `hms`.`t_room`
-DROP COLUMN `description`,
-ADD COLUMN `status` tinyint UNSIGNED NOT NULL DEFAULT 0 COMMENT '房间状态  0待租  1出租中  2待处理' AFTER `number`,
-ADD COLUMN `description` varchar(255) NOT NULL DEFAULT '' COMMENT '描述' AFTER `member_id`;
+ALTER TABLE `t_room` DROP COLUMN `description`;
+ALTER TABLE `t_room` ADD (`description` varchar(255) DEFAULT '' NOT NULL COMMENT '描述');
 
-ALTER TABLE `hms`.`t_member`
-ADD COLUMN `sex` tinyint(1) NOT NULL COMMENT '性别' AFTER `name`,
-MODIFY COLUMN `status` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'status：0正常  1退租' AFTER `room_id`;
-ALTER TABLE `hms`.`t_member`
-MODIFY COLUMN `sex` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '性别' AFTER `name`;
+ALTER TABLE `t_member` ADD (`sex` tinyint(1) NOT NULL COMMENT '性别');
+ALTER TABLE `t_member` ALTER COLUMN `status` tinyint(1) DEFAULT 0 NOT NULL COMMENT 'status：0正常  1退租';
+
+ALTER TABLE `t_member` ALTER COLUMN `sex` tinyint(1) DEFAULT 0 NOT NULL COMMENT '性别';
